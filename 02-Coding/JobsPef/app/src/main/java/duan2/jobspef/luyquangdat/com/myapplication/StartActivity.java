0d8fc261c2c6069ea1e92c366d5b04a542e65552
quang.nv <ngocquang994@gmail.com>
2017-07-08 13:22:14 +0700

    Tao 02-Coding

---
+++ 02-Coding/JobsPef/app/src/main/java/duan2/jobspef/luyquangdat/com/myapplication/StartActivity.java
@@ -0,0 +1,13 @@
+package duan2.jobspef.luyquangdat.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_start);
+    }
+}
